1ac21c5ef0e33ed3d6f49bb2224608cb30663e47
Mariano Dorneles <marianodorneles@gmail.com>
2020-06-16 02:53:19 -0300

    Update main.py
    Fixed UTC (in parts), better message formatting.
    
    Now shows all users status instead of one.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 # import math
 from dotenv import load_dotenv
 from discord.ext import commands
+from dateutil import tz
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,7 +19,6 @@ async def help_cmd(ctx):
     # for user in ctx.guild.members:
     # math.ceil(numpy.size(ctx.guild.members)/10)
     last_activity = []
-    lastest_activity = []
     channel_index = 0
     user_index = 0
     for user in ctx.guild.members:
@@ -29,14 +29,21 @@ async def help_cmd(ctx):
                         last_activity.append(message.created_at)
                         break
                 channel_index += 1
-        lastest_activity.insert(user_index, last_activity[0])
-        for activity in last_activity:
-            if activity > lastest_activity[user_index]:
-                lastest_activity.insert(user_index, activity)
-        user_ls = f'User {user} last activity on {lastest_activity[user_index]}'
-        last_activity = []
+
+        if last_activity:
+            lastest_activity = last_activity[0]
+            for activity in last_activity:
+                if activity > lastest_activity:
+                    lastest_activity = activity
+            from_zone = tz.tzutc()
+            to_zone = tz.tzlocal()
+            lastest_activity = lastest_activity.replace(tzinfo=from_zone)
+            lastest_activity = lastest_activity.astimezone(to_zone).strftime("%d/%m/%Y, %H:%M")
+            user_ls = f'User {user.mention} last activity on {lastest_activity}'
+        else:
+            user_ls = f'User {user.mention} has no recent activity on text channel'
         user_index += 1
+        last_activity = []
         await ctx.send(user_ls)
-        break
 
 bot.run(TOKEN)
