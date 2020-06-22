08973725de92e81b2eac92ab67e6018aca38bd14
Mariano Dorneles <marianodorneles@gmail.com>
2020-06-22 04:28:03 -0300

    Update main.py, requirements.txt
    The bot now displays users in pages, add error message.
    
    Removed unused requirements.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # bot.py
 import os
 
-# import discord
-# import numpy
-# import math
+import discord
+import numpy
+import math
 from dotenv import load_dotenv
 from discord.ext import commands
 from dateutil import tz
@@ -15,13 +15,16 @@ bot = commands.Bot(command_prefix='cs')
 
 
 @bot.command(name='userla', help="Shows the users last activity on server")
-async def help_cmd(ctx):
-    # for user in ctx.guild.members:
-    # math.ceil(numpy.size(ctx.guild.members)/10)
+async def userla(ctx, actual_page: int):
     last_activity = []
     channel_index = 0
     user_index = 0
-    for user in ctx.guild.members:
+    user_la = f""
+    num_pages = math.ceil(numpy.size(ctx.guild.members)/10)
+    offset = (10*(actual_page-1))
+    max_index = 10*actual_page
+    members = ctx.guild.members[offset:max_index]
+    for user in members:
         for channel in ctx.guild.channels:
             if channel.type[0] == 'text':
                 async for message in channel.history(limit=150):
@@ -29,7 +32,6 @@ async def help_cmd(ctx):
                         last_activity.append(message.created_at)
                         break
                 channel_index += 1
-
         if last_activity:
             lastest_activity = last_activity[0]
             for activity in last_activity:
@@ -39,11 +41,24 @@ async def help_cmd(ctx):
             to_zone = tz.tzlocal()
             lastest_activity = lastest_activity.replace(tzinfo=from_zone)
             lastest_activity = lastest_activity.astimezone(to_zone).strftime("%d/%m/%Y, %H:%M")
-            user_ls = f'User {user.mention} last activity on {lastest_activity}'
+            user_la += f'**{user.display_name}** last activity on {lastest_activity}\n'
         else:
-            user_ls = f'User {user.mention} has no recent activity on text channel'
+            user_la += f'**{user.display_name}** has no recent activity on text channel\n'
         user_index += 1
         last_activity = []
-        await ctx.send(user_ls)
+    embed = discord.Embed(title="Users last activity", color=0x85fd2c)
+    embed.add_field(name="User", value=user_la, inline=False)
+    embed.add_field(name="Page", value=f'{actual_page}/{num_pages}', inline=False)
+    embed.add_field(name="Help", value='To navigate through pages, use **csuserla _PAGE\_NUMBER_**', inline=False)
+    await ctx.send(embed=embed)
+
+
+@userla.error
+async def userla_error(ctx, error):
+    embed = discord.Embed(title="Oops...", color=0x85fd2c)
+    embed.add_field(name="Error", value="I've found an error on handling your request,"
+                                        " maybe you forgot to specify the page?", inline=False)
+    embed.add_field(name="Help", value='To navigate through pages, use **csuserla _PAGE\_NUMBER_**', inline=False)
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
