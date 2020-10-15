3c95941847b6073701c7d5c352bd2a3d7b1aed52
Mariano Dorneles <mdfreitas@inf.ufsm.br>
2020-10-15 04:37:38 -0300

    Renamed Main.py to Classer.py, added PrefHandler.py, added languages

--- Classer.py
+++ Classer.py
@@ -1,21 +1,22 @@
 # bot.py
-import os
-
 import discord
 import numpy
 import math
 from dotenv import load_dotenv
 from discord.ext import commands
 from dateutil import tz
+from PrefHandler import *
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix='cs-')
 
-bot = commands.Bot(command_prefix='cs')
 
-
-@bot.command(name='userla', help="Shows the users last activity on server")
+@bot.command(name='userla')
 async def userla(ctx, actual_page: int):
+    pref_handler = PrefHandler(ctx.guild.id)
+    server_timezone = pref_handler.get_timezone()
+    language = pref_handler.get_lang()
     last_activity = []
     channel_index = 0
     user_index = 0
@@ -40,25 +41,27 @@ async def userla(ctx, actual_page: int):
             from_zone = tz.tzutc()
             to_zone = tz.tzlocal()
             lastest_activity = lastest_activity.replace(tzinfo=from_zone)
-            lastest_activity = lastest_activity.astimezone(to_zone).strftime("%d/%m/%Y, %H:%M")
-            user_la += f'**{user.display_name}** last activity on {lastest_activity}\n'
+            lastest_activity = lastest_activity.astimezone(to_zone).strftime(language[0][2].text)
+            user_la += f'**{user.display_name}** '+language[0][4].text+f' {lastest_activity}\n'
         else:
-            user_la += f'**{user.display_name}** has no recent activity on text channel\n'
+            user_la += f'**{user.display_name}** '+language[0][6].text+'\n'
         user_index += 1
         last_activity = []
-    embed = discord.Embed(title="Users last activity", color=0x85fd2c)
-    embed.add_field(name="User", value=user_la, inline=False)
-    embed.add_field(name="Page", value=f'{actual_page}/{num_pages}', inline=False)
-    embed.add_field(name="Help", value='To navigate through pages, use **csuserla _PAGE\_NUMBER_**', inline=False)
+    embed = discord.Embed(title=language[0][0].text, color=0x85fd2c)
+    embed.add_field(name=language[0][3].text, value=user_la, inline=False)
+    embed.add_field(name=language[0][5].text, value=f'{actual_page}/{num_pages}', inline=False)
+    embed.add_field(name=language[0][7].text, value=language[0][8].text, inline=False)
     await ctx.send(embed=embed)
 
 
 @userla.error
 async def userla_error(ctx, error):
-    embed = discord.Embed(title="Oops...", color=0x85fd2c)
-    embed.add_field(name="Error", value="I've found an error on handling your request,"
-                                        " maybe you forgot to specify the page?", inline=False)
-    embed.add_field(name="Help", value='To navigate through pages, use **csuserla _PAGE\_NUMBER_**', inline=False)
+    pref_handler = PrefHandler(ctx.guild.id)
+    language = pref_handler.get_lang()
+
+    embed = discord.Embed(title=language[1][0].text, color=0x85fd2c)
+    embed.add_field(name=language[1][1].text, value=language[1][2].text, inline=False)
+    embed.add_field(name=language[1][3].text, value=language[1][4].text, inline=False)
     await ctx.send(embed=embed)
 
 bot.run(TOKEN)
---
+++ PrefHandler.py
@@ -0,0 +1,52 @@
+import xml.etree.ElementTree as ET
+import os
+import pandas as pd
+import shutil
+import subprocess
+
+
+def server_file_check(guild_id):
+    return os.path.isfile('servers/'+str(guild_id)+'.xml')
+
+
+class PrefHandler(object):
+
+    ##
+    # Handler initialization
+    # @param      guild_id    Guild ID
+    ##
+    def __init__(self, guild_id):
+        if not server_file_check(guild_id):
+            shutil.copy('servers/server_model.xml', 'servers/'+str(guild_id)+'.xml')
+
+        self.file_name = 'servers/'+str(guild_id)+'.xml'
+        self.tree = ET.parse(self.file_name)
+        self.root = self.tree.getroot()
+        df = pd.read_csv('languages/lang_dictionary.csv', dtype='string', encoding='utf-8').transpose().values.tolist()
+        self.languages = dict(zip(df[0], df[1]))
+
+    def get_lang(self):
+        for language in self.languages:
+            if self.languages[language] == self.root[0].text:
+                return ET.parse('languages/'+language+'.xml').getroot()
+
+        return None
+
+    def set_lang(self, new_lang):
+        if new_lang in self.languages.values():
+            self.root[0].text = new_lang
+            self.tree.write(self.file_name)
+            return 1
+        else:
+            return 0
+
+    def get_timezone(self):
+        return int(self.root[1].text)
+
+    def set_timezone(self, new_timezone):
+        if int(new_timezone) in range(-11, 14, 1):
+            self.root[1].text = new_timezone
+            self.tree.write(self.file_name)
+            return 1
+        else:
+            return 0
